e240844b480b8364463a10657eeb94a50029a1e3
rbaraglia <baraglia.rudy@gmail.com>
2018-10-05 11:42:46 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="linto_ui",
+    version="0.2.0",
+    include_package_data=True,
+    packages=find_packages(),
+    entry_points = {
+        'console_scripts': ['linto_ui=ui.linto_ui:main'],
+    },
+    install_requires=[
+        'pygame',
+        'tenacity',
+        'pyalsaaudio',
+        'paho-mqtt'
+    ],
+    package_data={},
+    author="Rudy Baraglia",
+    author_email="baraglia.rudy@gmail.com",
+    description="linto_ui is the GUI for the LinTo device",
+    license="AGPL V3",
+    keywords="GUI linto pygame",
+    url="",
+    project_urls={
+        "github" : ""
+    },
+    long_description="Refer to README"
+
+)
